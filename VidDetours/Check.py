019668ce58bdd7b8037da54965f6b1f7eda72977
Ruey Day <rueyday1@gmail.com>
2024-12-04 14:10:45 -0500

    Update Check.py

--- VidDetours/Check.py
+++ VidDetours/Check.py
@@ -1,2 +1,5 @@
-from transformers.models.bloom import modeling_bloom
-print(dir(modeling_bloom))
+try:
+    from transformers.models.bloom.modeling_bloom import _expand_mask as _expand_mask_bloom
+    print("Import successful!")
+except ImportError as e:
+    print(f"Import failed: {e}")
