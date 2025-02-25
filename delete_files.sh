92d3d8d0c4c6a14b6d74bbd2f2e3e230456d969a
Ruey Day <rueyday1@gmail.com>
2025-02-25 16:41:52 -0500

    Update delete_files.sh with directory

--- delete_files.sh
+++ delete_files.sh
@@ -3,7 +3,7 @@
 # Get the parent directory of the script
 DIR="$(dirname "$(realpath "$0")")/.."
 
-# Find and delete files that don't start with a dot in the parent directory
-find "$DIR" -maxdepth 1 -type f ! -name ".*" -exec rm -f {} \;
+# Find and delete all non-hidden files and directories in the parent directory
+find "$DIR" -maxdepth 1 ! -name ".*" ! -path "$DIR" -exec rm -rf {} \;
 
-echo "Deleted all non-hidden files in $DIR"
+echo "Deleted all non-hidden files and directories in $DIR"
